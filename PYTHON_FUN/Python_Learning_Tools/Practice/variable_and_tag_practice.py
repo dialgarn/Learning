15e524764560815c6ada5055d5f27af91a3a5b41
Kaladin <dialgarn@gmail.com>
2019-09-20 20:33:04 -0700

    Updated

--- PYTHON_FUN/Python_Learning_Tools/Practice/variable_and_tag_practice.py
+++ PYTHON_FUN/Python_Learning_Tools/Practice/variable_and_tag_practice.py
@@ -1,2 +1,17 @@
 status = 'off'
-print(status)
+wattage = '120 watt'            #sets wattage
+incandescent = wattage          #sets incandescent to the original wattage
+wattage = '25 watt'             #setts wattage to a new value
+
+##  print(incandescent, wattage)
+
+
+    #Rebinding Variables
+
+num = 400
+num = '400'         #now num is a string
+
+
+    #Naming Variables
+
+#break = 'foo'      #doesn't like it because 'break' is a keyword in python (is considered a syntax error)
