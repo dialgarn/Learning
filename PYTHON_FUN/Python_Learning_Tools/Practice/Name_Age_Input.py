ef85c1307627f99b6617e95db6d3e22bdfdda2cf
Kaladin <dialgarn@gmail.com>
2019-09-21 23:05:50 -0700

    finished identity type and value

--- PYTHON_FUN/Python_Learning_Tools/Practice/Name_Age_Input.py
+++ PYTHON_FUN/Python_Learning_Tools/Practice/Name_Age_Input.py
@@ -2,6 +2,8 @@ name = input('Enter your name:') + ','
 
 age = input('How old are you?:')
 
+print(type(age))
+
 print('Hey', name, 'you will be', int(age)+1, 'next year.')
 
 
--- PYTHON_FUN/Python_Learning_Tools/Practice/identity_type_value.py
+++ PYTHON_FUN/Python_Learning_Tools/Practice/identity_type_value.py
@@ -1,2 +1,34 @@
-name = 'Matt'
-print(id(name))
+#name = 'Matt'
+#print(id(name))
+#first = name
+#print(id(first))
+#print(first == name)
+
+    #lists are mutable
+
+#names = []
+#print(names)
+#print(id(names))
+
+#names.append('Fred')
+#print(names)
+#print(id(names))
+
+    #integers are not
+
+#age = 1000
+#print(id(age))
+#age = age + 1
+#print(id(age))
+
+    #Excercises
+
+pi = 3.14
+print(id(pi))
+print(type(pi))
+print(pi)
+
+pi = pi + 20
+print(id(pi))
+print(type(pi))
+print(pi)
