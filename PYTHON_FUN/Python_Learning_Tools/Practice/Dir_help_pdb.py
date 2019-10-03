c734919c21162a28edb5b309026335185b0a5d5b
Adolin <dialgarn@gmail.com>
2019-10-03 13:49:28 -0700

    finished with dir, help, and pdb

---
+++ PYTHON_FUN/Python_Learning_Tools/Practice/Dir_help_pdb.py
@@ -0,0 +1,24 @@
+#print('matt'.capitalize())
+#print('Hi {}'.format('there'))
+#print('YIKES'.lower())
+#print('hullo'.upper())
+
+#print(help('some string'.upper))
+
+
+    #EXERCISES
+
+#1
+#name = 'Devin'
+#print(dir(name))
+#print(help(''.find))
+#print(help(''.title))
+
+print('hello there my dear old friend'.title())
+
+#2
+
+age = 17
+print(dir(age))
+print(help(int.numerator))
+
