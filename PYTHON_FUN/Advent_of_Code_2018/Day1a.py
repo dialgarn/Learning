5655dd47a0996bfdc5d03efe7fae19a88192837a
Adolin <dialgarn@gmail.com>
2019-11-21 17:15:53 -0800

    finally finished day1a, now onto day1b

--- PYTHON_FUN/Advent_of_Code_2018/Day1a.py
+++ PYTHON_FUN/Advent_of_Code_2018/Day1a.py
@@ -1,4 +1,45 @@
-f = open("frequency_list.txt", "r")
-x = f.readlines()
-f.close()
-print(x.sum())
+fin = open("frequency_list2.txt")
+num_list = []
+for line in fin:
+    num_list.append(line)
+
+fin.close
+new_list = []
+for val in num_list[:-1]:
+    new_list.append(val[:-1])
+
+new_list.append(num_list[-1])
+final_sum = 0
+for val in new_list:
+    final_sum += int(val)
+
+print(final_sum)
+
+
+def frequency_sum(filename):
+    # takes input from a file and sums up the values
+    numList = []
+    newList = []
+    finalSum = 0
+    fin = open(filename)
+    for line in fin:
+        numList.append(line)
+    fin.close
+    for val in numList[:-1]:
+        newList.append(val[:-1])
+    newList.append(numList[-1])
+    for val in newList:
+        finalSum += int(val)
+    return finalSum
+
+
+print(frequency_sum('frequency_list.txt'))
+print(frequency_sum('frequency_list2.txt'))
+
+
+# Part B
+temp_sum = 0
+sum_list = []
+for val in new_list:
+    temp_sum += int(val)
+    sum_list.append(temp_sum)
